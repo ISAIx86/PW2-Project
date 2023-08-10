181df853d0a8c706b8b0db6114ec2d5b0864c96b
CYANx86 <isai.contrerasgrz@uanl.edu.mx>
2023-08-10 17:09:42 -0600

    Probando el webhook de discord. :3

--- server/server.js
+++ server/server.js
@@ -2,6 +2,6 @@ const express = require('express')
 
 const app = express()
 
-app.get('/endpoint', (req, res) => res.send('<h1>Esto es un endpoint!</h1>'))
+app.get('/endpoint', (req, res) => res.send('<h1>Esto es un endpoint</h1>'))
 
 app.listen(5005, () => console.log('SERVIDOR LEVANTADO'))
