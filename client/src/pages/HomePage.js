d1e3228d87d42963c65bd740e1acce5b01d372ae
orjamoon <orlando.moralesontv@uanl.edu.mx>
2023-10-04 22:02:15 -0600

    Home Page
    Aún no está terminada, falta acomodar lo de la clase post-bottom (que vendría siendo lo de likes y comentarios)
    Además hacer alguna validación para que verifique si el post pertenece a un videojuego o si es general

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,28 +1,105 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import axios from 'axios'
-import makeToast from '../plugins/Toaster'
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import * as BiIco from 'react-icons/bi';
+import '../styles/home.css';
 
-import 'bootstrap/dist/css/bootstrap.css'
-import '../styles/login.css'
 
 function Home() {
+  const [posts, setPosts] = useState([
+    {
+      id: 1,
+      game: {
+        name: 'Resident Evil 5',
+        avatar: 'https://th.bing.com/th/id/R.24d86c1cc073d119639092931f49ba20?rik=lxtvGV4Y8ioSEw&pid=ImgRaw&r=0'
+      },
+      user: {
+        name: 'Usuario 1',
+        avatar: 'https://th.bing.com/th/id/OIP.3hckg2BT4mldMAuuo6TUrQHaHa?w=202&h=202&c=7&r=0&o=5&dpr=1.3&pid=1.7',
+      },
+      content: 'Este es un comentario de prueba.',
+      media: 'https://th.bing.com/th/id/OIP.Hx0TNUPJV4BMBbWI516P2wHaEo?w=301&h=187&c=7&r=0&o=5&dpr=1.3&pid=1.7', // URL de la imagen o video
+      likes: 10,
+      comments: 5,
+      timestamp: 'Hace 2 horas',
+    },
+    {
+      id: 2,
+      game: {
+        name: '',
+        avatar: ''
+      },
+      user: {
+        name: 'Usuario 2',
+        avatar: 'https://th.bing.com/th/id/OIP.DMDfV6N9jhYjyQCLGLlH5QHaLH?pid=ImgDet&rs=1',
+      },
+      content: 'Otra publicación interesante.',
+      media: 'https://th.bing.com/th/id/R.24d86c1cc073d119639092931f49ba20?rik=lxtvGV4Y8ioSEw&pid=ImgRaw&r=0',
+      likes: 5,
+      comments: 3,
+      timestamp: 'Hace 3 horas',
+    },
+  ]);
 
-
-    return (
-        <div className='d-flex flex-column vh-100 align-items-center '>
-      <div className="header col-12 position-sticky">
+  return (
+    <div className='d-flex flex-column vh-100 align-items-center'>
+      <div className='header col-12 position-sticky'>
         <h1>BYTE-STREAM</h1>
       </div>
-      <div className="container d-flex justify-content-center mt-2 h-100">
-        <div className="login col-12 col-md-10 col-sm-10 col-xs-12">
-          <h2 className='mt-3'>Inicio</h2>
-          <hr />
+      <div className='container col-8 d-flex justify-content-center mt-2 mb-5 h-100'>
+        <div className=' col-12 col-md-10 col-sm-10 col-xs-12'>
+          {posts.map((post) => (
+            <div key={post.id} className='post-container mb-4'>
+              <div className='post-top'>
+                <div className='user-info'>
+                  <img
+                    src={post.user.avatar}
+                    alt={`Foto de perfil de ${post.user.name}`}
+                    className='profile-img'
+                  />
+                  <span className='username'>{post.user.name}</span>
+                </div>
+                <div className='right-info'>
+                  <div className='game-info'>
+                    <span className='username'>{post.game.name}</span>
+                    <img
+                      src={post.game.avatar}
+                      alt={`Foto del juego ${post.game.name}`}
+                      className='profile-img'
+                    />
+                  </div>
+                  <div className='dropdown'>
+                    <BiIco.BiDotsVerticalRounded className='dropdown-toggle drop-dots' data-bs-toggle='dropdown' aria-expanded='false' />
+                    <ul className='dropdown-menu'>
+                      <li><button className='dropdown-item' type='button'>Denunciar</button></li>
+                      <li><button className='dropdown-item' type='button'>Guardar</button></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <div className='post-body'>
+                <p className='post-content'>{post.content}</p>
+                <div className='d-flex justify-content-center'>
+                  <img src={post.media} alt='Media' className='post-media' />
+                </div>
+              </div>
+              <div className='post-actions post-bottom'>
+                <div className='post-activity'>
+                  <button className='like-button'>
+                    <i className='fa fa-heart' /> {post.likes}
+                  </button>
+                  <button className='comment-button'>
+                    <i className='fa fa-comment' /> {post.comments}
+                  </button>
+                </div>
+                <span className='timestamp'>{post.timestamp}</span>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
-    )
-
+  );
 }
 
-export default Home
+export default Home;
