c4ac151d917d3369165cd7595f10ee9192ac89e9
sebastianidiot0 <83084622+sebastianidiot0@users.noreply.github.com>
2023-10-07 20:22:35 -0600

    Perfil/Notificaciones
    Pagina de editar/borrar perfil basada en la pag de registro y Pagina de notificaciones basada en la pagina de home

--- client/src/pages/NotificationsPage.js
+++ client/src/pages/NotificationsPage.js
@@ -1,19 +1,90 @@
-import React from 'react'
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import * as BiIco from 'react-icons/bi';
+import '../styles/home.css';
 
-function Notifications() {
+
+function Notification() {
+  const [notif, setNotif] = useState([
+    {
+      id: 1,
+      game: {
+        name: 'Resident Evil 5',
+        avatar: 'https://th.bing.com/th/id/R.24d86c1cc073d119639092931f49ba20?rik=lxtvGV4Y8ioSEw&pid=ImgRaw&r=0'
+      },
+      user: {
+        name: 'Usuario 1',
+        avatar: 'https://th.bing.com/th/id/OIP.3hckg2BT4mldMAuuo6TUrQHaHa?w=202&h=202&c=7&r=0&o=5&dpr=1.3&pid=1.7',
+      },
+      content: 'Le dio like a tu publicacion.',
+      timestamp: 'Hace 2 horas',
+    },
+    {
+      id: 2,
+      game: {
+        name: '',
+        avatar: ''
+      },
+      user: {
+        name: 'Usuario 2',
+        avatar: 'https://th.bing.com/th/id/OIP.DMDfV6N9jhYjyQCLGLlH5QHaLH?pid=ImgDet&rs=1',
+      },
+      content: 'Comentó en tu publicación "Otra publicación interesante."',
+      timestamp: 'Hace 3 horas',
+    },
+  ]);
+
   return (
-    <div className='d-flex flex-column vh-100 align-items-center '>
-      <div className="header col-12 position-sticky">
+    <div className='d-flex flex-column vh-100 align-items-center'>
+      <div className='header col-12 position-sticky'>
         <h1>BYTE-STREAM</h1>
       </div>
-      <div className="container d-flex justify-content-center mt-2 h-100">
-        <div className="login col-12 col-md-10 col-sm-10 col-xs-12">
-          <h2 className='mt-3'>Notificaciones</h2>
-          <hr />
+      <div className='container col-8 d-flex justify-content-center mt-2 mb-5 h-100'>
+        <div className=' col-12 col-md-10 col-sm-10 col-xs-12'>
+          {notif.map((post) => (
+            <div key={post.id} className='post-container mb-4'>
+              <div className='post-top'>
+                <div className='user-info'>
+                  <img
+                    src={post.user.avatar}
+                    alt={`Foto de perfil de ${post.user.name}`}
+                    className='profile-img'
+                  />
+                  <span className='username'>{post.user.name}</span>
+                </div>
+                <div className='right-info'>
+                  <div className='game-info'>
+                    <span className='username'>{post.game.name}</span>
+                    <img
+                      src={post.game.avatar}
+                      alt={`Foto del juego ${post.game.name}`}
+                      className='profile-img'
+                    />
+                  </div>
+                  <div className='dropdown'>
+                    <BiIco.BiDotsVerticalRounded className='dropdown-toggle drop-dots' data-bs-toggle='dropdown' aria-expanded='false' />
+                    <ul className='dropdown-menu'>
+                      <li><button className='dropdown-item' type='button'>Marcar como leído</button></li>
+                      <li><button className='dropdown-item' type='button'>Reportar</button></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <div className='post-body'>
+                <p className='post-content'>{post.content}</p>
+               
+              </div>
+              <div className='post-actions post-bottom'>
+                
+                <span className='timestamp'>{post.timestamp}</span>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Notifications
+export default Notification;
--- client/src/pages/ProfilePage.js
+++ client/src/pages/ProfilePage.js
@@ -10,9 +10,97 @@ function Profile() {
         <div className="login col-12 col-md-10 col-sm-10 col-xs-12">
           <h2 className='mt-3'>Perfil</h2>
           <hr />
+          <div style={{display: 'flex', justifyContent: 'center'}}>
+          <img
+									src="./user-icon.svg"
+									alt="Imagen de usuario"
+									className="user-image"
+								/>
+
+
+     
+
+          </div>
+     <form>  
+      <div style={{display: 'flex', justifyContent: 'center'}}>
+							<input
+						className="login_form-campo col-lg-10 col-sm-10"
+            type="text"
+            id="username"
+            name="username"
+            placeholder="Nombre de usuario"
+            required
+							/>
+						</div>
+          <div style={{display: 'flex', justifyContent: 'center'}}>
+							<input
+								className="login_form-campo col-lg-10 col-sm-10"
+								type="text"
+								id="Nombres"
+								name="nombres"
+								placeholder="Nombres"
+								required
+							/>
+						</div>
+            <div style={{display: 'flex', justifyContent: 'center'}}>
+							<input
+								className="login_form-campo col-lg-10 col-sm-10"
+								type="text"
+								id="Apellidos"
+								name="apellidos"
+								placeholder="Apellidos"
+								required
+							/>
+						</div>
+            <div style={{display: 'flex', justifyContent: 'center'}}>
+							<input
+								className="login_form-campo col-lg-10 col-sm-10"
+								type="date"
+								id="fechaNac"
+								name="fecha_nac"
+								placeholder="Fecha de nacimiento"
+								required
+							/>
+						</div>
+            <div style={{display: 'flex', justifyContent: 'center'}}>
+							<input
+								className="login_form-campo col-lg-10 col-sm-10"
+								type="password"
+								id="password"
+								name="password"
+								placeholder="Contraseña"
+								required
+							/>
+						</div>
+            <div style={{display: 'flex', justifyContent: 'center'}}>
+							<input
+								className="login_form-campo col-lg-10 col-sm-10"
+								type="password"
+								id="confirmPass"
+								name="conf_password"
+								placeholder="Confirmar contraseña"
+								required
+							/>
+						</div>
+            <div  style={{display: 'flex', justifyContent: 'center'}} >
+              <button type="submit" className="login_form-button col-lg-4 col-xs-12 col-md-3 mb-3">
+							Cambiar
+						</button>
+            <button type="submit" className="login_form-button col-lg-4 col-xs-12 col-md-3 mb-3">
+							Eliminar
+						</button>
+            </div>
+            
+
+          </form>
+
         </div>
       </div>
     </div>
+  
+
+
+
   )
 }
 
